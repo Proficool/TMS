d843fac7d847132bff4c0d1dd2db885a8d044cc8
Vitaly Popkov <proficool@mail.com>
2024-11-19 13:42:51 +0300

    Изменение ДЗ5

--- dz_lesson_5.py
+++ dz_lesson_5.py
@@ -3,27 +3,42 @@
 # print(updated_url)
 
 def update_url():
-    url = "www.my_site.com#about"
     return url.replace("#", "/")
+url = str(input("Введите url c решеткой : "))
 print(update_url())
-
 
 # word = "stroka"
 # new_word = word + "ing"
 # print(new_word)
 
-def add_ing_to_word():
-    word = "stroka"
-    return word + "ing"
-print(add_ing_to_word())
+def add_word2_to_word1(word1, word2):
+    return word1 + word2
+word1 = str(input("Введите слово 1 : "))
+word2 = str(input("Введите слово 2 : "))
+print(add_word2_to_word1(word1, word2))  
 
 
 # colors = ["red", "blue", "green", "yellow", "purple"]
 # print(colors[1])
 
-colors = ["red", "blue", "green", "yellow", "purple"]
-def get_second_element(colors):
-    return colors[1]
-print(get_second_element(colors))  
+def get_color(colors, n):
+
+# Проверка, что индекс находится в пределах длины списка
+    if 0 <= n < len(colors):
+        return colors[n]
+    else:
+        return "Ошибка: индекс вне диапазона списка"
+
+# Ввод списка цветов 
+colors = input("Введите список цветов через запятую: ").split(",")
+
+# Убираем лишние пробелы вокруг цветов
+colors = [color.strip() for color in colors]
+
+# Ввод номера цвета 
+n = int(input(f"Введите номер цвета (0-{len(colors)-1}): "))
+
+# Вывод выбранного цвета
+print(f"Цвет под номером {n}: {get_color(colors, n)}")
 
 
